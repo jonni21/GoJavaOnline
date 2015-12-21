b5cdb2b4021403722b95d7f33017f0e5871b7ad2
jonni21 <jhnzvzd@gmail.com>
2015-12-21 15:50:29 +0200

    fields and methods were removed from the Flower class

--- src/gojava/module03/flower/Flower.java
+++ src/gojava/module03/flower/Flower.java
@@ -1,11 +1,4 @@
 package gojava.module03.flower;
 
 public abstract class Flower {
-
-    private double age;
-
-    public void plant() {}
-    public void pick() {}
-    public void pour() {}
-
 }
