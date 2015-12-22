4c4f1208eb8c215e641d82c20eb80e7891a1d956
jonni21 <jhnzvzd@gmail.com>
2015-12-22 15:38:51 +0200

    unnecessary variables were removed

--- src/gojava/module04/AreaCalculator.java
+++ src/gojava/module04/AreaCalculator.java
@@ -5,21 +5,18 @@ public class AreaCalculator {
     // Calculates triangle area using Heron's formula
     public double calculateTriangleArea(double firstSide, double secondSide, double thirdSide) {
         double semiperimeter = (firstSide + secondSide + thirdSide) / 2;
-        double triangleArea = Math.sqrt(semiperimeter * (semiperimeter - firstSide)
+        return Math.sqrt(semiperimeter * (semiperimeter - firstSide)
                 * (semiperimeter - secondSide) * (semiperimeter - thirdSide));
-        return triangleArea;
     }
 
     //Calculates rectangle area
     public double calculateRectangleArea(double firstSide, double secondSide) {
-        double rectangleArea = firstSide * secondSide;
-        return rectangleArea;
+        return firstSide * secondSide;
     }
 
     //Calculates circle area
     public double calculateCircleArea(double radius) {
-        double circleArea = Math.PI * Math.pow(radius, 2.0);
-        return circleArea;
+        return Math.PI * Math.pow(radius, 2.0);
     }
 
 }
--- src/gojava/module04/DistanceCalculator.java
+++ src/gojava/module04/DistanceCalculator.java
@@ -3,9 +3,8 @@ package gojava.module04;
 public class DistanceCalculator {
 
     public double calculateDistance(double pointX1, double pointY1, double pointX2, double pointY2) {
-        double distanceBetweenPoints = Math.sqrt(Math.pow(pointX2 - pointX1, 2.0)
+        return Math.sqrt(Math.pow(pointX2 - pointX1, 2.0)
                 + Math.pow(pointY2 - pointY1, 2.0));
-        return distanceBetweenPoints;
     }
 
 }
