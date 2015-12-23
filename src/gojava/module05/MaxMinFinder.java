fe69d0daecef6e7647d5f9ad38f0d11b50724884
jonni21 <jhnzvzd@gmail.com>
2015-12-23 15:33:41 +0200

    hometask for Module5 initial commit

---
+++ src/gojava/module05/MaxMinFinder.java
@@ -0,0 +1,26 @@
+package gojava.module05;
+
+import java.util.Arrays;
+
+public class MaxMinFinder {
+
+    public int findMax(int[] inputArray) {
+        int max = inputArray[0];
+        for (int element : inputArray) {
+            if (max < element) {
+                max = element;
+            }
+        }
+        return max;
+    }
+
+    public int findMin(int[] inputArray) {
+        int min = inputArray[0];
+        for (int element : inputArray) {
+            if (min > element) {
+                min = element;
+            }
+        }
+        return min;
+    }
+}
