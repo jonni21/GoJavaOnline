e8f25207221a217b1baaf0c7a4f4a2aaee2dd711
jonni21 <jhnzvzd@gmail.com>
2015-12-24 15:03:59 +0200

    moved tests to different package

--- src/gojava/test/SelectSortTest.java
+++ src/gojava/test/SelectSortTest.java
@@ -1,5 +1,6 @@
-package gojava.module05;
+package gojava.test;
 
+import gojava.module05.SelectSort;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,17 +11,17 @@ import static org.junit.Assert.*;
 public class SelectSortTest {
 
     @Test
-    public void ShouldSortArrayAscent() throws Exception {
+    public void shouldSortArrayAscent() throws Exception {
         SelectSort mySelectSort = new SelectSort();
         int[] arrayOfIntegers = {-23, 45, -99, 52, 0, 36, 41, 14, -425, 85, 41, 23, 7758, 54, 1};
         int[] expected = Arrays.copyOf(arrayOfIntegers, arrayOfIntegers.length);
         Arrays.sort(expected);
-        Assert.assertArrayEquals("failure - arrays not same", expected,
+        Assert.assertArrayEquals("failure - arrays are not same", expected,
                                  mySelectSort.sortAscent(arrayOfIntegers));
     }
 
     @Test
-    public void ShouldSortArrayDescent() throws Exception {
+    public void shouldSortArrayDescent() throws Exception {
         SelectSort mySelectSort = new SelectSort();
     }
 }
