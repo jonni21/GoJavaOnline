6c4968bc44183a4804389c0aef432343f2d5386f
jonni21 <jhnzvzd@gmail.com>
2016-01-29 14:39:22 +0200

    updated

--- src/gojava/module09/CaesarCipher.java
+++ src/gojava/module09/CaesarCipher.java
@@ -9,7 +9,7 @@ package gojava.module09;
 
 
 public class CaesarCipher {
-    /* To match letters to numbers we can use Unicode */
+    /* To match letters to the numbers we can use Unicode */
 
     /** The first letter 'a' matches to the corresponding Unicode code point  */
     final static private int FIRST_LETTER_CODE_POINT = "a".codePointAt(0);
@@ -34,13 +34,26 @@ public class CaesarCipher {
             }
         }
 
-
         return new String(cipherTextCodePoints, 0, cipherTextCodePoints.length);
-
     }
 
-    public static void  decode(String text, int key) {
+    public static String decode(String text, int key) {
+        int[] textCodePoints = new int[text.length()];
+        int[] cipherTextCodePoints = new int[text.length()];
 
+        for (int i = 0; i < text.length(); i++) {
+            cipherTextCodePoints[i] = text.codePointAt(i);
+        }
+
+        for (int i = 0; i < text.length(); i++) {
+            if ((cipherTextCodePoints[i] - key) >= FIRST_LETTER_CODE_POINT) {
+                textCodePoints[i] = cipherTextCodePoints[i] - key;
+            } else {
+                textCodePoints[i] = (LAST_LETTER_CODE_POINT + 1)
+                        - ((cipherTextCodePoints[i] + key) % LAST_LETTER_CODE_POINT);
+            }
+        }
+
+        return new String(textCodePoints, 0, textCodePoints.length);
     }
-
 }
